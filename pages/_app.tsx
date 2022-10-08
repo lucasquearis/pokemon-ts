0620126bb2c610a54950d6c2965ba86c45253ba0
lucasquearis <lucasquearis@hotmail.com>
2022-10-08 01:02:10 -0300

    feat: inicia projeto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React, { ReactNode } from "react";
+import Footer from "../templates/Footer";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): ReactNode {
+  return (
+    <div>
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ templates/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+function Footer(): ReactElement {
+  return (
+    <footer className="fixed bottom-0 w-screen h-10 bg-pink-400 flex items-center justify-center">
+      © 2022 - Lucas A. Santos - Todos os direitos reservados.
+    </footer>
+  );
+}
+
+export default Footer;
